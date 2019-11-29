b1171ee2545413232205187c716e55420fc6e16e
shanmuga369 <55756248+shanmuga369@users.noreply.github.com>
2019-11-29 23:43:59 +0530

    Update App.java

--- src/main/java/com/shanthemonk/app/App.java
+++ src/main/java/com/shanthemonk/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! I am a new dreamer" );
         System.out.println("This is my new project in devops");
     }
 }
